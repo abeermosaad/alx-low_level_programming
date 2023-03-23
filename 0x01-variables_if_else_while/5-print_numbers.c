74025fab3b7929978e7fbb05152a2226b95e931f
abeermosaad <abeermosaad69@gmail.com>
2023-03-23 13:32:53 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print the alphabet in lowercase and uppercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+
+	}
+
+	putchar('\n');
+	return (0);
+}
