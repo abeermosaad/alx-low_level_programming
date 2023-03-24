1c3afd33922ce7e651ce506dc44956edda2e7174
abeermosaad <abeermosaad69@gmail.com>
2023-03-24 05:42:25 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print the alphabet in lowercase and uppercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		putchar(a);
+		putchar(',');
+		putchar(' ');
+		a++;
+
+
+	}
+	putchar('\n');
+
+	return (0);
+
+}
