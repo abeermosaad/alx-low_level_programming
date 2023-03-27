e9eb86793d28dff772ebff36b81d0025fea6213a
abeermosaad <abeermosaad69@gmail.com>
2023-03-27 11:28:51 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to the standard output
+ *
+ * @c: the character to write
+ *
+ * Return: on success, the number of characters written
+ *         on error, -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+
+}
