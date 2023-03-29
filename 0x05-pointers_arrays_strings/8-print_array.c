723fa8e6c6342725973fa4e0baca5bbf50bb6dc1
abeermosaad <abeermosaad69@gmail.com>
2023-03-28 18:57:24 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,6 +14,10 @@
  */
 void print_array(int *a, int n)
 {
+	if (n <= 0)
+		printf("\n");
+	else
+	{
 	int i;
 
 	for (i = 0; i < n - 1; i++)
@@ -21,5 +25,5 @@ void print_array(int *a, int n)
 		printf("%d, ", *(a + i));
 	}
 	printf("%d\n", *(a + (n - 1)));
-
+	}
 }
