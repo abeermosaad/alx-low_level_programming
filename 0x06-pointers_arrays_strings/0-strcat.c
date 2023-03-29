811ac0962f768cb1f4266b77388460112513b57e
abeermosaad <abeermosaad69@gmail.com>
2023-03-29 15:32:48 -0700

    m

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,15 +4,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
+        int i=0;
+        int end = (int)strlen(dest);
 
-	int end = (int)strlen(dest);
-        
 
-	while (src[i] != '\0')
-	{
-		dest[end + i] = src[i];
-		i++;
-	}	
-	return (dest);
+        while (src[i] != '\0')
+        {
+                dest[end + i] = src[i];
+                i++;
+        }
+        dest[end + i] = '\0';
+
+        return (dest);
 }
