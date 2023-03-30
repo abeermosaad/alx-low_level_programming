d61297bf988b5a7f0dab44c835f8e88df49f9c24
abeermosaad <abeermosaad69@gmail.com>
2023-03-30 13:04:33 -0700

    _strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,13 +19,18 @@ char *_strncat(char *dest, char *src, int n)
 
 	int end = (int)strlen(dest);
 
+	int end2 = (int)strlen(src);
+
+	if (n > end2)
+	{
+		n = end2;
+
+	}
 	for (i = 0; i < n; i++)
 	{
 		dest[end + i] = src[i];
-		i++;
 
 	}
-	dest[end + i] = '\0';
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 #include <stdio.h>
 
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 #endif
