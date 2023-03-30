f1e174a566ba8d8309ff07ce344834e5d04aa629
abeermosaad <abeermosaad69@gmail.com>
2023-03-30 14:35:06 -0700

    compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - compares two strings.
+ *
+ * @s1: parameter
+ *
+ * @s2: parameter
+ *
+ * Return: on success,
+ *         on error, -1 is returned, and errno is set appropriately
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, result, end1 = (int)strlen(s1);
+
+	for (i = 0; s1[i] && s2[i] && i < end1; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			result = s1[i] - s2[i];
+			return (result);
+
+		}
+
+	}
+	return (0);
+
+}
