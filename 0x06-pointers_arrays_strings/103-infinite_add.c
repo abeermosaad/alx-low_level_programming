02d25c749cfe13382f586216515e257a55e69853
abeermosaad <abeermosaad69@gmail.com>
2023-04-01 19:09:30 -0700

    updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <string.h>
-
+#include <stdlib.h>
 char *infinite_add(char *n1, char *n2, char *r, int size_r){
-    int res = atoi(n1) + atoi (n2);
+    long  res = atoi(n1) + atoi (n2);
     int cnt = 0;
     int res2 = res;
      while (res != 0) {
@@ -11,7 +11,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r){
      }
     cnt++;
     if (cnt <= size_r){
-        for(int i = cnt - 2; i >= 0; i--)
+	    int i;
+        for(i = cnt - 2; i >= 0; i--)
         {
             r[i]= (res2 % 10) + '0';
             res2 /= 10;
