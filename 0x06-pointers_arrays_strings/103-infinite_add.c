40d12e38de3100aaf0a920961b138ed68aa4b605
root <root@DESKTOP-FRQDFCP.localdomain>
2023-04-03 01:17:16 +0200

    updated

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,24 +1,44 @@
 #include "main.h"
 #include <string.h>
 #include <stdlib.h>
-char *infinite_add(char *n1, char *n2, char *r, int size_r){
-    long  res = atoi(n1) + atoi (n2);
-    int cnt = 0;
-    int res2 = res;
-     while (res != 0) {
-          cnt++;
-        res /= 10;
-     }
-    cnt++;
-    if (cnt <= size_r){
-	    int i;
-        for(i = cnt - 2; i >= 0; i--)
-        {
-            r[i]= (res2 % 10) + '0';
-            res2 /= 10;
-        }
-        return (r);
-    }
-    else
-        return(0);
+
+/**
+ * infinite_add - adds two numbers
+ *
+ * @n1: First num
+ *
+ * @n2: Second num
+ *
+ * @r : string that will store the result on it
+ *
+ * @size_r : size of the string of result
+ *
+ * Return: on sucess will return result.
+*/
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	long  res = atoi(n1) + atoi(n2);
+	int cnt = 0;
+	int res2 = res;
+
+	while (res != 0)
+	{
+		cnt++;
+		res /= 10;
+	}
+	cnt++;
+	if (cnt <= size_r)
+	{
+		int i;
+
+		for (i = cnt - 2; i >= 0; i--)
+		{
+			r[i] = (res2 % 10) + '0';
+			res2 /= 10;
+		}
+		return (r);
+	}
+	else
+		return (0);
 }
