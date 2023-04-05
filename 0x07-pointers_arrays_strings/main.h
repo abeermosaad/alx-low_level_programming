32c87b0caeb2592b89d3acfd76c6330d4967e460
abeermosaad <abeermosaad69@gmail.com>
2023-04-05 21:25:20 +0200

    puts_recursion

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,4 +11,5 @@ char *_strstr(char *haystack, char *needle);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char a);
+void print_diagsums(int *a, int size);
 #endif
