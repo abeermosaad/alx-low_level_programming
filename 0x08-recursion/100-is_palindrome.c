427b7c66d859f989c250cc2e42aeafa8aed816c0
abeermosaad <abeermosaad69@gmail.com>
2023-04-06 03:01:40 +0200

    is_palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ *
+ * @s: the string
+ *
+ * Return: 0 or 1
+*/
+int is_palindrome(char *s)
+{
+	int i, flag = 1;
+
+	for (i = 0; i < ((int)strlen(s) / 2); i++)
+	{
+		if (s[i] != s[(int)strlen(s) - 1 - i])
+		{
+			flag = 0;
+			break;
+		}
+	}
+	if (flag)
+		return (flag);
+	else
+		return (flag);
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ *
+ * @x: the num
+ *
+ * @y: the power
+ *
+ * Return: the result
+*/
+int wildcmp(char *s1, char *s2)
+{
+    int len1 = (int)strlen(s1), len2 = (int)strlen(s2);
+
+    if (len1 != len2)
+        return (0);
+
+}
