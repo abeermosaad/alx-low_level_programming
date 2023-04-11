212d813faa89f8943d170cf1ddb1a26070be9fb9
abeermosaad <abeermosaad69@gmail.com>
2023-04-11 02:01:03 +0200

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: The size of the argv array
+ * @argv: An array containing the program command line arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
