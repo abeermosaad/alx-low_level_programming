9993fcb71e7d8142efd750704ff1417b5c711c14
abeermosaad <abeermosaad69@gmail.com>
2023-04-11 02:25:21 +0200

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: The size of the argv array
+ * @argv: An array containing the program command line arguments
+ * Return: Always 0.
+ */
+int main(int argc, char  __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
