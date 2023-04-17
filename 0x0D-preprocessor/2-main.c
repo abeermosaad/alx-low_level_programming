4bd4c76fb80e1381a5ac88a24792045d0dd98f1c
abeermosaad <abeermosaad69@gmail.com>
2023-04-17 21:05:44 +0200

    preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,12 +2,11 @@
 
 /**
  * main - check the code
- * @argc: The size of the argv array
- * @argv: An array containing the program command line arguments
+ *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	printf("%s\n", argv[argc - 1]);
+	printf("%s\n", __FILE__);
 	return (0);
 }
