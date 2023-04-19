405fabbdefd88e4450a9fe75ac8eaa0c8e9c83b8
abeermosaad <abeermosaad69@gmail.com>
2023-04-20 00:20:24 +0200

    function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (!action || !size)
+	if (!action || !array)
 		exit(-1);
 	for (i = 0; i < (int)size; i++)
 		action(array[i]);
