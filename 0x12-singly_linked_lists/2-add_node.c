e03a780ff2f4b2d243ce4eca1284514a2a7eea7e
abeermosaad <abeermosaad69@gmail.com>
2023-04-28 03:31:43 +0200

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_node - .
+ * @head: head
+ * @str: string
+ * Return: Always cnt.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = malloc(sizeof(list_t));
+	char *new_str;
+
+	if (ptr == NULL)
+		return (NULL);
+	new_str = strdup(str);
+
+	ptr->next = *head;
+	ptr->str = new_str;
+	*head = ptr;
+	return (*head);
+}
