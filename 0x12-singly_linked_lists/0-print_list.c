874dc277a30c91e4b1339eee5322efc7a67f6766
abeermosaad <abeermosaad69@gmail.com>
2023-04-28 02:22:48 +0200

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - rints all the elements of a list_t list.
+ * @h: head
+ * Return: Always cnt.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int cnt;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		cnt++;
+	}
+	return (cnt);
+}
