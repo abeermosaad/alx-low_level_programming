68b5683b8af83ff4ec300d3fc5d8fdc549774f14
abeermosaad <abeermosaad69@gmail.com>
2023-05-01 09:52:40 +0200

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head : the head of listint_t list.
+ * Return: none :)
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr, *temp;
+
+	ptr = *head;
+	while (ptr)
+	{
+		temp = ptr->next;
+		free(ptr);
+		ptr = temp;
+	}
+	*head = NULL;
+}
