693528f02fd1ca71e152abd561f84687a2ab6b67
abeermosaad <abeermosaad69@gmail.com>
2023-05-03 02:55:31 +0200

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * pop_listint - frees a listint_t list.
+ * @head: the head of listint_t list.
+ * Return: head node’s data (n) or 0 :)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	if (!(*head)->next)
+		return (0);
+	*head = (*head)->next;
+	return (temp->n);
+}
