6382a980ed795f8eb80d246739e3fb55c1649ec8
abeermosaad <abeermosaad69@gmail.com>
2023-05-05 08:01:14 +0200

    bit_manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit at a given index.
+ * @n: num
+ * @index: the idx of bit
+ * Return: value or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int cnt = 0;
+	int value, flag = 0;
+
+	while (n)
+	{
+		if (cnt == index)
+		{
+			if (n & 1)
+				value = 1;
+			else
+				value = 0;
+			flag = 1;
+			break;
+		}
+		n = n >> 1;
+		cnt++;
+	}
+	if (!flag)
+		return (-1);
+	return (value);
+}
