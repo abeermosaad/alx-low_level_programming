9ceb63e26b83b516dbb6761e07c686594d75df7f
abeermosaad <abeermosaad69@gmail.com>
2023-07-09 02:06:02 +0300

    task 6

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,47 +7,56 @@
  * Return: newly created hash table.
 */
 
-
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *item;
-	hash_node_t *curr;
 	unsigned long int index;
 
-	item = malloc(sizeof(hash_node_t));
+	if (ht == NULL)
+		return (0);
 
-	if (!item || ht == NULL)
-		return (0);
 	if (key == NULL || strlen(key) == 0)
-	{
-		free(item);
 		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	item = add_node(&(ht->array[index]), key, value);
+	if (!item)
+		return (0);
+	return (1);
+}
+/**
+ * add_node - .
+ * @head: head
+ * @key: string
+ * @value: string
+ * Return: Always cnt.
+ */
+
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *item;
+
+	item = *head;
+
+	while (item)
+	{
+		if (strcmp(item->key, key) == 0)
+		{
+			free(item->value);
+			item->value = strdup(value);
+			return (*head);
+		}
+		item = item->next;
 	}
+
+	item = malloc(sizeof(hash_node_t));
+	if (item == NULL)
+		return (NULL);
+
 	item->key = strdup(key);
 	item->value = strdup(value);
-	item->next = NULL;
-
-	index = key_index((const unsigned char *) item->key, ht->size);
-	if (ht->array[index] != NULL)
-	{
-		curr = ht->array[index];
-		while (curr)
-		{
-			if (strcmp(curr->key, key) == 0)
-			{
-				free(item->value);
-				free(item->key);
-				free(item);
-				curr->value = strdup(value);
-				return (1);
-			}
-			curr = curr->next;
-		}
-		item->next = ht->array[index];
-		ht->array[index] = item;
-	}
-	else
-		ht->array[index] = item;
-	return (1);
+	item->next = *head;
+	*head = item;
+	return (*head);
 }
