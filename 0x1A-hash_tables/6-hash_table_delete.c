062cf9e62536d582163e8ad58a46e533fd0c9e80
abeermosaad <abeermosaad69@gmail.com>
2023-07-09 00:13:32 +0300

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function that prints a hash table.
+ * @ht: hash_table
+ * Return: NONE.
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr, *temp;
+
+	if (!ht)
+		return;
+	
+	for (i = 0; i < (ht->size); i++)
+	{
+		if (ht->array[i])
+		{
+			curr = ht->array[i];
+			while (curr)
+			{
+				temp = curr;
+				free(temp->key);
+				free(temp->value);
+				curr = curr->next;
+			}
+			ht->array[i] = NULL;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
