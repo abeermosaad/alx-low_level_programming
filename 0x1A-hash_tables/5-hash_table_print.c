76b4a26cf0c288b330b37e4acbedd5506e914bc4
abeermosaad <abeermosaad69@gmail.com>
2023-07-08 06:01:46 +0300

    task 5 >> betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - function that creates a hash table.
- * @size: The size of the array.
- * Return: newly created hash table.
+ * hash_table_print - function that prints a hash table.
+ * @ht: hash_table
+ * Return: NONE.
 */
 
 void hash_table_print(const hash_table_t *ht)
@@ -10,21 +10,21 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *curr;
 	unsigned long int flag;
-	
+
 	for (i = (ht->size); (int)i >= 0; i--)
 	{
-		if(ht->array[i])
+		if (ht->array[i])
 		{
 			flag = i;
 			break;
 		}
 	}
-	
+
 	i = 0;
 	printf("{");
 	for (i = 0; i < (ht->size); i++)
 	{
-		if(ht->array[i])
+		if (ht->array[i])
 		{
 			curr = ht->array[i];
 			while (curr)
